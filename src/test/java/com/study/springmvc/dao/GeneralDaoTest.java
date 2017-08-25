6b3bc0c00aaf574d391d0fbe7b0a7b927fec6aa0
史保密 <baomi.shi@shenmajr.com>
2017-08-25 15:53:49 +0800

    测试类

--- src/test/java/com/study/springmvc/dao/GeneralDaoTest.java
+++ src/test/java/com/study/springmvc/dao/GeneralDaoTest.java
@@ -1,35 +1,64 @@
 package com.study.springmvc.dao;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-
-import org.springframework.util.FileCopyUtils;
 
 public class GeneralDaoTest {
 	public static void main(String[] args) {
+		copyDaoToService();
+	}
+	
+	public static void copyDaoToService() {
 		String projectPath=new File("").getAbsolutePath();
-		String daoPath="/src/main/java/com/study/springmvc/dal/faces/busi/product";
-		String servicePath="/src/main/java/com/study/springmvc/service/faces/busi/product/";
-		File daoFile=new File(projectPath+daoPath);
+		String daoPath=projectPath+"/src/main/java/com/study/springmvc/dal/faces/busi/product";
+		String servicePath=projectPath+"/src/main/java/com/study/springmvc/service/faces/busi/product/";
+		String destPackagePath="com.study.springmvc.service.faces.busi.product";
+		copyDaoDirctoryToServiceDirctory(daoPath,servicePath,destPackagePath);
+	}
+	
+	//拷贝文件，并替换包地址和类名
+	public static void copyDaoDirctoryToServiceDirctory(String srcPath,String destPath,String destPackagePath) {
+		File daoFile=new File(srcPath);
 		if(daoFile.isDirectory()) {
 			int i=0;
 			File[]daoInterface =daoFile.listFiles();
 			for(File f:daoInterface ) {
 				String oldFileName=f.getName();
-				File out=new File(projectPath+servicePath+oldFileName.replaceAll("Dao", "Service"));
+				File copyFile=new File(destPath+oldFileName.replaceAll("Dao", "Service"));
 				try {
-					if(out.exists()) {
+					if(copyFile.exists()) {
 						continue;
 					}
-					FileCopyUtils.copy(f, out);
+					BufferedReader in=new BufferedReader(new  FileReader(f));
+					BufferedWriter out=new BufferedWriter(new FileWriter(copyFile));
+					copyDaoInterfaceFileToServiceInterfaceFile(in,out,destPackagePath,f.getName());
+					in.close();
+					out.close();
 					i++;
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
 			}
-			System.out.println(i);
+			System.out.println("总共copy的文件："+i);
 		}else {
-			System.out.println("路径错误");
+			System.out.println("路径错误:"+srcPath);
 		}
+	}
+	
+	public static void copyDaoInterfaceFileToServiceInterfaceFile(BufferedReader in, BufferedWriter out,String destPackagePath,String oldFileName) throws IOException {
+		String line=null;
+		while((line=in.readLine())!=null) {
+			if(line.matches("^\\s?package\\s+.+")) {
+				line="package "+destPackagePath+" ;";
+			}else if(line.matches("^\\s?public\\s+interface\\s+.+")) {
+				line="public interface " + oldFileName.replace("Dao", "Service")+" {";
+			}
+			out.write(line);
+		}
+		out.flush();
 	}
 }
